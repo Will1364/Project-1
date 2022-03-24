10c3d3697c7dfaa4d1c068a7cab65c27d4ad071f
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-03-24 10:48:10 +0100

    Update Statistik.py

--- Statistik.py
+++ Statistik.py
@@ -9,20 +9,21 @@ with with numpy as np
 
 def dataStatistics(data, statistic):
     
-    dataT =data.t
+    dataT =data.t   #matricen bliver transponeret til en 3xN matrice så temperaturen vil være i øverste række, growth rate vil være i midten og bacterietypen vil være i nederste række.
     
+    #hvad dette stykke kode skal udregne bliver bestemt via en række if-sætninger
     if statistic == "Mean Temperature"
-        result = np.mean(dataT[0,:])
+        result = np.mean(dataT[0,:])    #mean temperature bliver beregnet ved at finde gennemsnittet af elementerne i øverste række
     elif statistic == "Mean Growth rate"
-        result = np.mean(dataT[1,:])
+        result = np.mean(dataT[1,:])    #mean growth rate bliver beregnet ved at finde gennemsnittet af elementerne i midterste række
     elif statistic == "Rows"
-        result = np.size(dataT[0,:])
+        result = np.size(dataT[0,:])    #antallet af rows i data bliver beregnet som længden af den øverste række
     elif statistic == "Std Temperature"
-        result = np.std(dataT[0,:])
+        result = np.std(dataT[0,:])     #std temperature er std deviation af temperaturmålingerne og det bliver udregnet via np.std af den øverste række af dataT
     elif statistic == "Std Growth rate"
-        result = np.std(dataT[1,:])
+        result = np.std(dataT[1,:])     #std growth rate er std deviation af temperaturmålingerne og det bliver udregnet via np.std af den øverste række af dataT
     elif statistic == "Mean Cold Growth rate"
-        result ==
+        result =
         
         
     return result
