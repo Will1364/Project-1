55e94aa208d594ba6ad1fb106b4f175ca5ad14b6
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-04-07 10:08:24 +0200

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -55,6 +55,29 @@ def dataLoad(filename):
 print(dataLoad("test.txt"))
 
 
+def dataStatistics(data, statistic):
+    
+    dataT =data.t #matricen bliver transponeret til en 3xN matrice så temperaturen vil være i øverste række, growth rate vil være i midten og bacterietypen vil være i nederste række.
 
+    #hvad dette stykke kode skal udregne bliver bestemt via en række if-sætninger
+    if statistic == "Mean Temperature"
+        result = np.mean(dataT[0,:])    #mean temperature bliver beregnet ved at finde gennemsnittet af elementerne i øverste række
+    elif statistic == "Mean Growth rate"
+        result = np.mean(dataT[1,:])    #mean growth rate bliver beregnet ved at finde gennemsnittet af elementerne i midterste række
+    elif statistic == "Rows"
+        result = np.size(dataT[0,:])    #antallet af rows i data bliver beregnet som længden af den øverste række
+    elif statistic == "Std Temperature"
+        result = np.std(dataT[0,:])     #std temperature er std deviation af temperaturmålingerne og det bliver udregnet via np.std af den øverste række af dataT
+    elif statistic == "Std Growth rate"
+        result = np.std(dataT[1,:])     #std growth rate er std deviation af temperaturmålingerne og det bliver udregnet via np.std af den øverste række af dataT
+    elif statistic == "Mean Cold Growth rate"
+        v = dataT[1,:][dataT[0,:] < 20] #der bliver skabt en vektor med alle de værdier for growth rate som har samme plads som       
+        result = np.mean(v)
+    elif statistic == "Mean Hot Growth rate"
+        v = dataT[1,:][dataT[0,:] > 20]                
+        result = np.mean(v)
+
+        
+    return result
 
     
