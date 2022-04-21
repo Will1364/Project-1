47376ba85fcaf950e83e699c858686d76b0eebf0
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-04-21 10:08:22 +0200

    Update Project1.py
    Jeg har implemeteret den del af koden der laver plots samt tilhørende brugergrænseflade

--- Project1.py
+++ Project1.py
@@ -20,12 +20,7 @@ class missingData(Exception): #Error type defineret for tilfælde hvor brugeren 
 class wrongString(Exception): #Error type defineret for tilfælde hvor brugeren skriver et input forkert
     pass
 status = 1
-
-
-
-
-        
-    
+           
 ####################################################################################################################################################################
 #Denne del indlæser datafil og omdanner  til matrice
 
@@ -105,8 +100,110 @@ def dataFilter(filtertype, betingelse):
         data = V
         
 
+####################################################################################################################################################################
+#denne del laver plots
+
+def dataPlot(data):
+    
+    Saen=data[data[:,2]==1]
+    Saen=data[data[:,2]==1]
+    Bace=data[data[:,2]==2]
+    Li=data[data[:,2]==3]
+    Brth=data[data[:,2]==4]
+    
+    
+    langs=['Salmonella','Bacillus','Listeria','Brochothrix']
+  
+    number=[np.sum(Saen[:,1]),np.sum(Bace[:,1]),np.sum(Li[:,1]),np.sum(Brth[:,1])]
+    
+    plt.bar(langs,number,align='center',alpha=0.5)
+    #plt.xticks(langs, y)
+    plt.ylabel('Amount')
+    plt.title('Number og bacteria')
+    plt.show()
+
+#plot 2
+ #Næste plot af de fire grafer for growth rate iforhold til temp
+    
+def dataPlot2(data):
+            temp = np.array(data[:,0]) #Finder temperturen i dataen
+            growth = np.array(data[:,1]) #Finder growth rate dataen
+            
+            temp1 = np.ones(np.size(temp),dtype=bool)# Laver en vektor med størrelse 1
+            growth1 = np.ones(np.size(growth),dtype=bool)# Laver en vektor med størrelse 1
+            
+        #Det samme gentages for de andre temperaturer, samt bakterier
+            
+            temp2 = np.ones(np.size(temp),dtype=bool) 
+            growth2 = np.ones(np.size(growth),dtype=bool) 
+            temp3 = np.ones(np.size(temp),dtype=bool) 
+            growth3 = np.ones(np.size(growth),dtype=bool)
+            temp4 = np.ones(np.size(temp),dtype=bool) 
+            growth4 = np.ones(np.size(growth),dtype=bool) 
 
 
+            for i in range(len(temp)):
+                if not (data[i,2] == 1):
+                    temp1[i] = False #Dem der ikke er lig 1 er false
+                    growth1[i] = False #Dem der ikke er lig 1 er false
+            temp1x = np.array(temp[temp1]) #x-værdi for bakterie 1
+            growth1y = np.array(growth[growth1]) #y-værdi for bakterie 1
+
+            for i in range(len(temp)): #Loopet gentages
+                if not (data[i,2] == 2):
+                    temp2[i] = False
+                    growth2[i] = False
+            temp2x = np.array(temp[temp2]) #x-væriden for bakterie 2
+            growth2y = np.array(growth[growth2]) #y-værdien for bakterie 2
+
+            for i in range(len(temp)): #Loopet gentages
+                if not (data[i,2] == 3):
+                    temp3[i] = False
+                    growth3[i] = False
+            temp3x = np.array(temp[temp3])  # x-væriden for bakterie 3
+            growth3y = np.array(growth[growth3]) # y-værdien for bakterie 3
+            
+            for i in range(len(temp)): #Loopet gentages
+                if not (data[i,2] == 4):
+                    temp4[i] = False
+                    growth4[i] = False
+            temp4x = np.array(temp[temp4])  # x-væriden for bakterie 4
+            growth4y = np.array(growth[growth4]) # y-værdien for bakterie 4
+
+    # Sorterer de forskellige rækker data
+            sortorder1 = np.argsort(temp1x)
+            temp1x = temp1x[sortorder1] 
+            growth1y = growth1y[sortorder1] 
+            sortorder2 = np.argsort(temp2x) 
+            temp2x = temp2x[sortorder2] 
+            growth2y = growth2y[sortorder2] 
+            sortorder3 = np.argsort(temp3x)
+            temp3x = temp3x[sortorder3] 
+            growth3y = growth3y[sortorder3] 
+            sortorder4 = np.argsort(temp4x) 
+            temp4x = temp4x[sortorder4] 
+            growth4y = growth4y[sortorder4] 
+  
+    #Plotter plottet
+    #Plottet punkterne på grafen
+            plt.scatter(temp1x, growth1y,color="purple")
+            plt.scatter(temp2x, growth2y,color="cyan")
+            plt.scatter(temp3x, growth3y,color="blue")
+            plt.scatter(temp4x, growth4y,color="magenta")
+            plt.title('Growth rate by temperature') #Titel
+            plt.xlabel('Temperature') #Navn x-aksen
+            plt.ylabel('Growth rate') #Navn y-aksen
+            plt.xlim([10, 60]) #Sætter interval på x-aksen
+            plt.ylim([0,1]) #Sætter interval på y-aksen
+            plt.plot(temp1x, growth1y, label = 'Salmonella', color="purple") 
+            plt.plot(temp2x, growth2y, label = 'Bacillus', color="cyan") 
+            plt.plot(temp3x, growth3y, label = 'Listeria', color="blue") 
+            plt.plot(temp4x, growth4y, label = 'Brochothrix', color="magenta") 
+            plt.legend(loc="upper right")
+            plt.show() 
+        
+        
+        
 
 ####################################################################################################################################################################
 #Her er den aktive del af koden
@@ -207,6 +304,36 @@ while status == 1: #programmet vil altid vende tilbage til hovedmenuen, ved mind
             
               """)    
     
+    
+    if Command == "Generer diagrammer":
+        print("""Du kan nu vælge følgende grafer:     #undermenu viser brugeren hvilke former for statistik programmet kan fremstille
+        - Number of bacteria
+        - Growth rate by temperature
+        
+        - Tilbage""")
+        while True:
+            
+            try:
+                plotType = str(input("Indtast venligst dit valg:"))      #programmet beder brugeren vælge en statistik
+                if plotType != "Number of bacteria" and plotType != "Growth rate by temperature" and plotType != "Tilbage":
+                    raise wrongString    #hvis brugeren intaster en forkert string vil der blive kaldt en NameError                                                           
+                break
+            except wrongString:
+                print("Dette input er ikke gyldigt. Tjek evt. for stavefejl og prøv igen.")
+            except ValueError:
+                print("Dette input er ikke gyldigt. Input skal være tekst")
+        if  plotType == "Number of bacteria"
+            print(dataPlot(data))
+        elif plotType == "Growth rate by temperature"
+            print(dataPlot2(data))
+            time.sleep(3)
+        print("""
+            
+            
+              """)    
+    
+    
+    
         
     ####################################################################################################################################################################
     #Programmet afsluttes
@@ -215,5 +342,5 @@ while status == 1: #programmet vil altid vende tilbage til hovedmenuen, ved mind
     if Command == "Afslut":
         status = 0
       
-      
+    
             
